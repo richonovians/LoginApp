d56feb087932a52b2290270292a174743257b0eb
richonovians <richonov@gmail.com>
2025-02-26 22:33:45 +0700

    modifikasi pindah activity

--- app/src/main/java/com/example/loginapp/HomeActivity.kt
+++ app/src/main/java/com/example/loginapp/HomeActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
